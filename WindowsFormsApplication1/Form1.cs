c8f15bd07274fe7be5be8938a28e1117d5be2a24
Drofko <sven270290@yandex.ru>
2016-12-24 10:03:10 +0300

    lab5

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,25 +19,27 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
-
-            Random rnd = new Random();
-            int number;
-            int count = 0;
-
-            for (int index = 0; index < 10; index++)
             {
-                number = -100 + rnd.Next(200);
-                listBox1.Items.Add(number);
-            }
+                listBox1.Items.Clear();
 
-            for (int index = 1; index < 9; index += 2)
-            {
-                if (((int)listBox1.Items[index] > (int)listBox1.Items[index + 1]) && ((int)listBox1.Items[index] > (int)listBox1.Items[index - 1]))
-                    count++;
+                Random rnd = new Random();
+                int number;
+                int count = 0;
+
+                for (int index = 0; index < 10; index++)
+                {
+                    number = -100 + rnd.Next(200);
+                    listBox1.Items.Add(number);
+                }
+
+                for (int index = 0; index < 9; index++)
+                {
+                    if ((int)listBox1.Items[index] * (int)listBox1.Items[index + 1] < 0)
+                        count++;
+                }
+
+                MessageBox.Show(count.ToString());
             }
-            MessageBox.Show(count.ToString());
         }
     }
-}
 
